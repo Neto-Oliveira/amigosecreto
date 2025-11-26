fcdef636325a24136fb83fc804c00a90ac4f8753
Neto-Oliveira <neto.oliveira.lr4@gmail.com>
2025-11-26 13:01:43 -0300

    versão 2

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const URL_API = "https://script.google.com/macros/s/AKfycbyOMndTED6B5cx0mRIq4fG3Ju4pN2bbwOPEes5JpIvy1Ue2ZPtIUKc9DN5eq3cvzYY/exec";
+
 document.getElementById("btn").onclick = async () => {
+  const nomeInformado = prompt("Digite APENAS o seu primeiro nome:");
+
+  if (!nomeInformado || nomeInformado.trim() === "") {
+    alert("Você precisa informar seu nome!");
+    return;
+  }
+
   const btn = document.getElementById("btn");
   btn.disabled = true;
   btn.innerText = "Carregando...";
 
   try {
-    const res = await fetch(URL_API);
+    const res = await fetch(`${URL_API}?nome=${encodeURIComponent(nomeInformado)}`);
     const data = await res.json();
 
-    if (data.status === "empty") {
-      alert("Todos os participantes já foram sorteados!");
-      return;
+    if (data.status === "ja_sorteou") {
+      alert("Você já fez seu sorteio!");
+    } 
+    else if (data.status === "ok") {
+      document.getElementById("nome").innerText = data.sorteado;
+      document.getElementById("card").style.display = "block";
+    } 
+    else {
+      alert(data.message || "Erro inesperado.");
     }
 
-    document.getElementById("nome").innerText = data.nome;
-    document.getElementById("cep").innerText = data.cep;
-    document.getElementById("endereco").innerText = data.endereco;
-
-    document.getElementById("card").style.display = "block";
   } catch (err) {
     console.log(err);
-    alert("Erro ao chamar a API.");
+    alert("Erro ao chamar API.");
   }
+
+  btn.disabled = false;
+  btn.innerText = "Descobrir Amigo Secreto";
 };
